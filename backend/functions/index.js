4acc7b9debacefbd8bb3ea70796a8cbce72cd995
Alex Krantz <alex@krantz.dev>
2020-05-30 11:34:29 -0700

    Add authentication verification

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,8 +1,33 @@
-const functions = require('firebase-functions');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const httpContext = require("express-http-context");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+const app = express();
+app.use(cors({ origin: true }))
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use((req, res, next) => {
+        // Ensure token exists
+        if (req.get("Authorization") === "" || req.get("Authorization") === undefined) {
+            res.status(401).json({ status: "error", reason: "unauthorized" });
+            return;
+        }
+
+        // TODO: handle anonymous users
+
+        // Validate token
+        admin.auth().verifyIdToken(req.get("Authorization"))
+            .then(decoded => {
+                httpContext.set("uid", decoded.uid);
+                next();
+            })
+            .catch(_ => res.status(401).json({ status: "error", reason: "unauthorized" }));
+    })
+    .all("*", (_, res) => res.status(404).json({ status: "error", reason: "not found" }));
+
+exports.api = functions.https.onRequest(app);
