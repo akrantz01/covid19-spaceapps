f798bf14fdea4ec4ef98cf4f18fe57ba7c9df312
yuyuan luo <luo.yuyuan@yahoo.com>
2020-05-30 17:21:22 -0400

    headers!

--- profile/profile.js
+++ profile/profile.js
@@ -12,11 +12,3 @@ $(document).ready(function() {
     document.getElementById('WELLNESS').style.display = 'none';
 });
 
-
-function swap(elementid) {
-      document.getElementById(elementid).src = "assets/health_cup_full.png"; 
-}
-
-document.getElementById("cup1").addEventListener("click", function() {
-    swap("cup1");
-}, false);
