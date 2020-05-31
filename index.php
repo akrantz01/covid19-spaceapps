cc5f3e18db76463cb5ef3f6067bb5cd8eb0cab0c
Andre Nonaka <42698192+Andre-Nonaka@users.noreply.github.com>
2020-05-31 11:40:55 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <html>
  <head>
   <link rel="stylesheet" href="css/style3.css">
-  <title>Comment System using PHP and Ajax</title>
+  <title>CSSI Comments</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
