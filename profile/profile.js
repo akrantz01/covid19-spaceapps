ebf32f1ccd085afb62e5fb9832349626eab946dc
Tiffany <cnnmonn@gmail.com>
2020-05-30 21:46:50 -0400

    location

--- profile/profile.js
+++ profile/profile.js
@@ -1,38 +1,68 @@
 function displayFeed() {
-    $("#FEED").show();
-    $("#WELLNESS").hide();
-    $("#feed_button").css("opacity", 1);
-    $("#wellness_button").css("opacity", 0.3);
-  }
+  $("#FEED").show();
+  $("#WELLNESS").hide();
+  $("#feed_button").css("opacity", 1);
+  $("#wellness_button").css("opacity", 0.3);
+}
 
 function displayWellness() {
-    $("#WELLNESS").show();
-    $("#wellness_button").css("opacity", 1);
-    $("#feed_button").css("opacity", 0.3);
-    $("#FEED").hide();
-    $("#pol").hide();
+  $("#WELLNESS").show();
+  $("#wellness_button").css("opacity", 1);
+  $("#feed_button").css("opacity", 0.3);
+  $("#FEED").hide();
+  $("#pol").hide();
 }
 
 $(document).ready(function() {
-    displayFeed();
+  displayFeed();
 });
 
 //backend people: change the following function for pollution detection!
 function chngimg() {
-    if (true) { //In particular, change this!!! if pollution low.
-        $("#tree").css("opacity", 1);
-        $("#house").css("opacity", 0.3);
-        $("#pol").hide();
-        $("#nopol").show();
-    }
-     else {
-        $("#tree").css("opacity", 0.3);
-        $("#house").css("opacity", 1);
-        $("#pol").show();
-        $("#nopol").hide();
-    }
+  if (true) { //In particular, change this!!! if pollution low.
+    $("#tree").css("opacity", 1);
+    $("#house").css("opacity", 0.3);
+    $("#pol").hide();
+    $("#nopol").show();
+  } else {
+    $("#tree").css("opacity", 0.3);
+    $("#house").css("opacity", 1);
+    $("#pol").show();
+    $("#nopol").hide();
+  }
 }
 
 function goHome() {
   window.location.href = "../index.html";
+}
+
+//LOCATION SCRIPTS//
+function getLocation() {
+  if (navigator.geolocation) navigator.geolocation.getCurrentPosition(showPosition, showError);
+  else alert("Geolocation is not supported by this browser.");
+}
+
+function showPosition(position) { //change later
+  localStorage.setItem('pos', position.coords);
+  $("#location").css("opacity", 1);
+  let posString = "(" + Math.round(position.coords.latitude) +
+  "," + Math.round(position.coords.longitude) + ")";
+  $("#location p").html(posString);
+}
+
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      console.log("User denied the request for Geolocation.");
+      break;
+    case error.POSITION_UNAVAILABLE:
+      console.log("Location information is unavailable.");
+      break;
+    case error.TIMEOUT:
+      console.log("The request to get user location timed out.");
+      break;
+    case error.UNKNOWN_ERROR:
+      console.log("An unknown error occurred.");
+      break;
+  }
 }
